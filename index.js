52c4cd1f75ae4a71b07888254567827e15dbfbe9
dongwonKim <dongwon90.kim@gmail.com>
2019-09-22 00:33:43 +0900

    Add middleware

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = (req, res) => {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
 
 const handleHome = (req, res) => {
-    res.send("Home");
-}
+  res.send("Home");
+};
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", handleHome);
 
