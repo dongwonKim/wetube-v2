052aa9390a94cbe1852b6799cab535c29c772a0b
dongwonKim <dongwon90.kim@gmail.com>
2019-09-22 16:26:08 +0900

    Home controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:1232141,
+        title : "Video awsome",
+        description: "This is thing what I want",
+        views : 24,
+        videoFile : "",
+        creator : {
+            id:23232,
+            name:"dongwon",
+            email:"asdfasf@asdfasdF.com"
+        }
+    },
+    {
+        id:123,
+        title : "Video love",
+        description: "This is thing what I want",
+        views : 24,
+        videoFile : "",
+        creator : {
+            id:23232,
+            name:"dongwon",
+            email:"asdfasf@asdfasdF.com"
+        }
+    },
+    {
+        id:12321,
+        title : "Video funny",
+        description: "This is thing what I want",
+        views : 24,
+        videoFile : "",
+        creator : {
+            id:23232,
+            name:"dongwon",
+            email:"asdfasf@asdfasdF.com"
+        }
+    },
+    {
+        id:123214,
+        title : "Video good",
+        description: "This is thing what I want",
+        views : 2,
+        videoFile : "",
+        creator : {
+            id:23332,
+            name:"dongwon",
+            email:"asdfasf@asdfasdF.com"
+        }
+    }
+]
