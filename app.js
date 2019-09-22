818887dd3b2e881e5b8df014c83affa518843523
dongwonKim <dongwon90.kim@gmail.com>
2019-09-22 12:01:07 +0900

    template variables

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
-import helmet from "helmet";
 import morgan from "morgan";
+import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -10,11 +11,15 @@ import routes from "./routes";
 
 const app = express();
 
+app.set("view engine", "pug");
+
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- init.js
+++ init.js
@@ -2,6 +2,6 @@ import app from "./app";
 
 const PORT = 4000;
 
-const handleListening = (req, res) => console.log(`Listening on : http://localhost${PORT}`);
+const handleListening = (req, res) => console.log(`Listening on : http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
